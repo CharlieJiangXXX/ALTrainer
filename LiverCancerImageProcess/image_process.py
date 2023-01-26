436335ef938b1751e49e209d7344c6b294b4afdf
cjiang <45895922+CharlieJiangXXX@users.noreply.github.com>
2023-01-26 09:22:23 +0800

    Fixup image processing.

--- LiverCancerImageProcess/image_process.py
+++ LiverCancerImageProcess/image_process.py
@@ -1,31 +1,32 @@
 import os
 import pickle
 import numpy as np
+from tqdm import tqdm
 
 
-def gen_dict(root_dir):
-    d = {"labels": [], "data": np.array([], dtype=np.uint8), "filenames": []}
+def gen_dict(root_dir) -> dict:
+    out_dict = {"labels": [], "data": np.array([], dtype=np.uint8), "filenames": []}
 
     def scan_dir(label, dir):
-        for file in os.listdir(dir):
-            d["labels"].append(label)
+        for file in tqdm(os.listdir(dir)):
+            out_dict["labels"].append(label)
             with open(dir + '/' + file, "rb") as image:
                 b = bytearray(image.read())
                 if b:
-                    d["data"] = np.append(d["data"], b[0])
-            d["filenames"].append(file)
+                    out_dict["data"] = np.append(out_dict["data"], b)
+            out_dict["filenames"].append(file)
 
     for name in os.listdir(root_dir):
         full_name = root_dir + '/' + name
         if os.path.isdir(full_name):
             if name == "Normal_Processed":
+                print("Processing normal...")
                 scan_dir(0, full_name)
             elif name == "Rat_HCC_HE_Processed":
+                print("Processing rat HCC HE...")
                 scan_dir(1, full_name)
-            else:
-                continue
-
-    return d
+            continue
+    return out_dict
 
 
 def dump_dict(root_dir):
@@ -38,7 +39,6 @@ def unpickle_dict(file):
     with open(file, 'rb') as fo:
         d = pickle.load(fo, encoding='bytes')
     return d
-
 
 dump_dict("/media/cjiang/Extreme SSD")
 
